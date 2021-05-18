f5180bd988074dc8945a557ced426739a1c5b98f
Michael Davey <mikey@new-signal.com>
2021-05-18 16:48:50 +0100

    Make history data more resilient

--- history.py
+++ history.py
@@ -11,12 +11,16 @@ class History:
         if not self._history_file.is_file():
             self._history_file.touch()
             self._data = {'readings': []}
+            self._write_data()
         else:
             self._data = yaml.safe_load(open(self._history_file))
 
     def add_reading(self, value):
         self._data['readings'].insert(0, value)
         self._data['readings'] = self._data['readings'][:self.SIZE]
+        self._write_data()
+
+    def _write_data(self):
         with open(self._history_file, "w") as file:
             file.write(yaml.dump(self._data))
 
